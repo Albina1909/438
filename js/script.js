ace2e97a7262e9a06954afcf3620cda3904d1206
Albina <aaa-19091969@rambler.ru>
2021-05-11 13:47:53 +0300

    Решена задача №2

--- js/script.js
+++ js/script.js
@@ -1,26 +1,66 @@
-let num=100;
-
-let kursDollara =74.8;
-let kursEuro = 90.5;
-
-let sumDollars= 0, sumEuro=0;
-
-let rubles = prompt("Сумма в рублях");
-
-if (!rubles && typeof rubels == "Object") {
-  alert("Вы отменили!!!");}
- else{ 
-  if (rubles =="") {alert("Вы ничего не ввели!!!");}
-
-  // else {isNaN(+ rubels) ? alert("Это не число") : alert (`Сумма в $ = ${rubels/kursDollara},
-  //   Сумма в евро = ${rubels/kursEuro}`);}}
 
 
-  else 
-  {if ( isNaN (+rubles)) { alert("Это не число");}
-  else { sumDollars =(rubles/kursDollara).toFixed(2);
-   sumEuro = (rubles/kursEuro).toFixed(2); 
-alert(`Сумма в долларах  = ${sumDollars}$, сумма в евро = ${sumEuro}\u20AC` );}}}
+
+
+
+
+let a,b; //стороны трапеции
+let h; //высота трапеции
+let s; //искомая площадь трапеции
+
+let result = "";// строка с результатом
+
+a=+prompt("Сторона а:");
+b=+prompt("Сторона b:");
+h=+prompt("Высота h:");
+
+if (a > 0 && b > 0 && h > 0) { S = ((a + b) / 2) * h; result = `Площадь трапеции со сторонами a = ${a} , b = ${b}\n и высотой h = ${h} равна ${S}` }
+
+else{result = `Неверные данные!!!Площадь невозможно посчитать`;}
+alert(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num=100;
+
+// let kursDollara =74.8;
+// let kursEuro = 90.5;
+
+// let sumDollars= 0, sumEuro=0;
+
+// let rubles = prompt("Сумма в рублях");
+
+// if (!rubles && typeof rubels == "Object") {
+//   alert("Вы отменили!!!");}
+//  else{ 
+//   if (rubles =="") {alert("Вы ничего не ввели!!!");}
+
+//   // else {isNaN(+ rubels) ? alert("Это не число") : alert (`Сумма в $ = ${rubles/kursDollara},
+//   //   Сумма в евро = ${rubles/kursEuro}`);}}
+
+
+//   else 
+//   {if ( isNaN (+rubles)) { alert("Это не число");}
+//   else { sumDollars =(rubles/kursDollara).toFixed(2);
+//    sumEuro = (rubles/kursEuro).toFixed(2); 
+// alert(`Сумма в долларах  = ${sumDollars}$, сумма в евро = ${sumEuro}\u20AC` );}}}
 
 
 
