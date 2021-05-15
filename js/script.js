5db8e7ac1d2910dc4c5396ec65ab35540110baf9
Albina <aaa-19091969@rambler.ru>
2021-05-15 17:01:45 +0300

    Пример на использование do-while и switch (задача мини-калькулятор)

--- js/script.js
+++ js/script.js
@@ -1,30 +1,141 @@
-//***Задача№3***/
+// Мини-калькулятор
+// сложение 2-х чисел
+// разность 2-х чисел
+// произведение 2-х чисел
+// частное 2-х чисел
+// возведение числа в энную степень
+// корень энной степени вычислить
+let cancel = true;
+let menu = ` Ваш выбор (0-6)": 
+1. Сложение 2-х чисел
+2. Разность 2-х чисел
+3. Произведение 2-х чисел
+4. Деление 2-х чисел
+5. Возведение числа в n-степень
+6. Вычисление корня n-степени
+0. Отмена `;
+let num1, num2;
+let result;
 
-let sum; // сумма вклада
-let percent; // годовой %
-let countYears; // количество лет
-let result; // итоговая строка с выводом
+do {
+  let operation = prompt(menu, "0");
+  
+  switch(operation){
+    case "1" :
+       
+      num1 = +prompt("Первое число:");
+      num2 = +prompt("Второе число:");
+                      result = num1 + num2;
+      alert(`Сумма чисел = ${result}`);
+              break;
+    case "2": 
+      num1 = prompt("Первое число:");
+      num2 = prompt("Второе число:");
+      result = num1 - num2;
+      alert(`Разность чисел = ${result}`);
 
-let endWordYears = " "; // строка, содержащая "год(а)","лет"
+              break; 
+    case "3": 
 
-sum = Number(prompt("Сумма вклада:"));
-percent = Number(prompt("Годовой %:"));
-countYears = Number(prompt("Период вклада(лет)"));
+      num1 = prompt("Первое число:");
+      num2 = prompt("Второе число:");
+      result = num1 * num2;
+      alert(`Произведение чисел = ${result}`);
+              break;
+    case "4":
+     
+      num1 = prompt("Первое число:");
+      num2 = prompt("Второе число:");
+      if (num2 != 0) {
+      result = num1 / num2;
+      alert(`Частное 2-х чисел = ${result .toFixed(2)}`);}
+      else { alert(`Делить на 0 нельзя}`);}
 
-for (let i = 1; i <= countYears; i++) {
-  sum += sum * percent / 100;
+              break;
+    case "5": 
+      num1 = prompt("Число:");
+      num2 = prompt("Степень:");
 
-if (i != 11 && i % 10 == 1) {endWordYears = "год";}
+      if (num2 < 0) { result = Math.pow(num1, num2).toFixed (2);}
+      else{
+      // result = num1 ** num2;
+      result = Math.pow(num1, num2);}
+      alert(`Число ${ num1} в степени ${num2} = ${result}`);
 
-  else if ((i >= 5 && i <= 20) ||( i % 10 >= 5 && i % 10 <=9) || (i % 10) ==0)
-  { endWordYears = "лет";}
+              break;
+    case "6": num1 = prompt("Число:");
+      num2 = prompt("Степень корня:");
+      result = Math.pow(num1, 1 / num2 );
+      alert(`Корень степени ${num2} из числа ${num1} = ${result}`);
+  
+      // if (num2 < 0) { result = Math.pow(num1, num2).toFixed(2); }
+      // else {
+      //   // result = num1 ** num2;
+      //   result = Math.pow(num1, num2);
+      // }
+      // alert(`Число ${num1} в степени ${num2} = ${result}`);
 
-else { endWordYears = "года";}
-result += `через ${i} ${endWordYears} сумма = ${sum.toFixed (2)}\n`;
+              break;
+    case "0": 
+    case null: cancel = false;
+               break;
+    default: alert("Ошибка! Неверный выбор!!!");
+        
+              break;
+  }
+} while (cancel);
+  
 
-console.log(`через ${i} ${endWordYears} сумма = ${sum.toFixed (2)}`);}
 
-alert(result);
+
+// Задача5 
+// let i = 0 ;
+// // цикл с предусловием
+// while (i<=50){ i ++;
+//   // if (i ==25){break;}
+//   if (i == 25) { continue; }
+//   console.log(i+ `\n`);}
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i == 25) { continue; }
+//   console.log(i + `\n`);}
+// цикл с постусловием
+// do {
+//   console.log(i + `\n`);
+//   i++;}
+ 
+// while (i <= 50);
+// while можно замнить на for (let i = 51 ;i <= 50; i ++){}
+
+
+
+// //***Задача№3***/
+
+// let sum; // сумма вклада
+// let percent; // годовой %
+// let countYears; // количество лет
+// let result; // итоговая строка с выводом
+
+// let endWordYears = " "; // строка, содержащая "год(а)","лет"
+
+// sum = Number(prompt("Сумма вклада:"));
+// percent = Number(prompt("Годовой %:"));
+// countYears = Number(prompt("Период вклада(лет)"));
+
+// for (let i = 1; i <= countYears; i++) {
+//   sum += sum * percent / 100;
+
+// if (i != 11 && i % 10 == 1) {endWordYears = "год";}
+
+//   else if ((i >= 5 && i <= 20) ||( i % 10 >= 5 && i % 10 <=9) || (i % 10) ==0)
+//   { endWordYears = "лет";}
+
+// else { endWordYears = "года";}
+// result += `через ${i} ${endWordYears} сумма = ${sum.toFixed (2)}\n`;
+
+// console.log(`через ${i} ${endWordYears} сумма = ${sum.toFixed (2)}`);}
+
+// alert(result);
 
   // //если
   //   if (условие) {// команды
